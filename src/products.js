39502104130c9cec5ab360c9309554eac9c537c0
Hoang Nguyen <90513580+NguyenVIetHoang57@users.noreply.github.com>
2023-04-08 11:56:47 +0700

    Fix

--- src/products.js
+++ src/products.js
@@ -2,11 +2,7 @@ import product1 from "./assets/products/1.png";
 import product2 from "./assets/products/2.png";
 import product3 from "./assets/products/3.png";
 import product4 from "./assets/products/4.png";
-import product5 from "./assets/products/5.png";
-import product6 from "./assets/products/6.webp";
-import product7 from "./assets/products/7.webp";
-import product8 from "./assets/products/8.webp";
-import product9 from "./assets/products/9.webp";
+
 
 export const PRODUCTS = [
   {
@@ -33,30 +29,7 @@ export const PRODUCTS = [
     price: 228.0,
     productImage: product4,
   },
-  {
-    id: 5,
-    productName: "LED Light Strips",
-    price: 19.99,
-    productImage: product5,
-  },
-  {
-    id: 6,
-    productName: "SPECTRUM LS TEE",
-    price: 68.0,
-    productImage: product6,
-  },
-  {
-    id: 7,
-    productName: "AUTO SERVICE SHIRT by GOLF WANG",
-    price: 120.0,
-    productImage: product7,
-  },
-  {
-    id: 8,
-    productName: "DON'T TRIP UNSTRUCTURED HAT",
-    price: 40.0,
-    productImage: product8,
-  },
+
 
 
 ];
